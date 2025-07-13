ea3b048ec32e8478901abbd1f090e6525506bed3
Muskan3404 <aggarwal3404@gmail.com>
2025-07-13 17:18:02 +0530

    backend complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,41 @@
-import express from "express"
+import express, { application } from "express"
 import { ENV } from "./config/env";
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express";
 import {connectDB} from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
+const port = ENV.PORT || 3000;
 const app=express();
-
-// Add essential middleware
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.get("/", (req, res) => res.send("Hello"));
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // Start server with proper async/await handling
 const startServer = async () => {
   try {
     await connectDB();
-    const port = ENV.PORT || 3000;
-    app.listen(port, () => {
+    
+    if (ENV.NODE_ENV != "production"){
+      app.listen(port, () => {
       console.log(`Server running on port ${port} ✅`);
     });
+
+    }
+    
   } catch (error) {
     console.error("Failed to start server:", error.message);
     process.exit(1);
@@ -25,3 +43,5 @@ const startServer = async () => {
 };
 
 startServer();
+
+export default app;
